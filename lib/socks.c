d3ae0e80695047738d29e1ffd8f1e98abeb779a8
CrackedPoly <crackedpoly@outlook.com>
2022-07-03 15:45:13 +0800

    enable logging remote address in socks5 replies

--- lib/socks.c
+++ lib/socks.c
@@ -774,6 +774,42 @@ CURLcode Curl_SOCKS5(const char *proxy_name,
   }
   else {
     infof(data, "SOCKS5 request granted.\n");
+    if(socksreq[3] == 1) {
+      infof(data,
+            "Complete SOCKS5 connection to %d.%d.%d.%d:%d. (%d)\n",
+            (unsigned char)socksreq[4], (unsigned char)socksreq[5],
+            (unsigned char)socksreq[6], (unsigned char)socksreq[7],
+            (((unsigned char)socksreq[8] << 8) |
+             (unsigned char)socksreq[9]),
+            (unsigned char)socksreq[1]);
+    }
+    else if(socksreq[3] == 3) {
+      unsigned char port_upper = (unsigned char)socksreq[len - 2];
+      socksreq[len - 2] = 0;
+      infof(data,
+            "Complete SOCKS5 connection to %s:%d. (%d)\n",
+            (char *)&socksreq[5],
+            ((port_upper << 8) |
+             (unsigned char)socksreq[len - 1]),
+            (unsigned char)socksreq[1]);
+      socksreq[len - 2] = port_upper;
+    }
+    else if(socksreq[3] == 4) {
+      infof(data,
+            "Complete SOCKS5 connection to %02x%02x:%02x%02x:"
+            "%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%d. (%d)\n",
+            (unsigned char)socksreq[4], (unsigned char)socksreq[5],
+            (unsigned char)socksreq[6], (unsigned char)socksreq[7],
+            (unsigned char)socksreq[8], (unsigned char)socksreq[9],
+            (unsigned char)socksreq[10], (unsigned char)socksreq[11],
+            (unsigned char)socksreq[12], (unsigned char)socksreq[13],
+            (unsigned char)socksreq[14], (unsigned char)socksreq[15],
+            (unsigned char)socksreq[16], (unsigned char)socksreq[17],
+            (unsigned char)socksreq[18], (unsigned char)socksreq[19],
+            (((unsigned char)socksreq[20] << 8) |
+             (unsigned char)socksreq[21]),
+            (unsigned char)socksreq[1]);
+    }
   }
 
   (void)curlx_nonblock(sock, TRUE);
